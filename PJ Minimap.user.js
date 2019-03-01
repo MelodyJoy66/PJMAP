e863036293dfd585b5cd7161149706673ad140d7
MelodyJoy66 <48119989+MelodyJoy66@users.noreply.github.com>
2019-03-01 11:00:35 +0200

    Update PJ Minimap.user.js

--- PJ Minimap.user.js
+++ PJ Minimap.user.js
@@ -6,9 +6,9 @@
 // @author       ConsoleBey#9737
 // @match        https://pixelzone.io/*
 // @match        http://pixelzone.io/*
-// @homepage     https://github.com/MelodyJoy6/PJMAP
-// @updateURL    https://github.com/MelodyJoy6/PJMAP/blob/master/PJ%20Minimap.user.js
-// @downloadURL  https://github.com/MelodyJoy6/PJMAP/blob/master/PJ%20Minimap.user.js
+// @homepage     https://github.com/MelodyJoy66/PJMAP
+// @updateURL    https://github.com/MelodyJoy66/PJMAP/edit/master/PJ%20Minimap.user.js
+// @downloadURL  https://github.com/MelodyJoy66/PJMAP/edit/master/PJ%20Minimap.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -18,7 +18,7 @@ Number.prototype.between = function(a, b) {
   return this > min && this < max;
 };
 var range = 25;
-window.baseTepmlateUrl = 'https://github.com/MelodyJoy6/PJMAP';
+window.baseTepmlateUrl = 'https://github.com/MelodyJoy66/PJMAP';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
